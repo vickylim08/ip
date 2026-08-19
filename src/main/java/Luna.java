da631cbb25e1d344e5a2fc5bf6c112585c97db76
Vicky Lim <vickylim078@gmail.com>
2026-08-19 19:36:43 +0800

    Add the ability to store text users input and display back as list

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Luna {
@@ -9,8 +11,14 @@ public class Luna {
                 "____________________________________________________________\n";
         System.out.println(banner);
         Scanner scanner = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
 
-        System.out.println("Type anything (type 'bye' to exit):");
+        System.out.println(
+                "Available Commands:\n" +
+                        "<any text>: adds a new item to your task list\n" +
+                        "list: displays all currently saved items with index numbers\n" +
+                        "bye: Exits the program\n"
+        );
         String exit =
                 "   ____________________________________________________________\n" +
                 "       Bye. Hope to see you again soon!\n" +
@@ -19,15 +27,22 @@ public class Luna {
         while (true) {
             System.out.print("> ");
             String input = scanner.nextLine().trim();
-            String echo =
+            String response =
                     "   ____________________________________________________________\n" +
-                    "       " + input + "\n" +
+                    "       added: " + input + "\n" +
                     "   ____________________________________________________________";
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println(exit);
                 break;
-            } else {
-                System.out.println(echo);
+            } else if (input.equalsIgnoreCase("list")) {
+                System.out.println("   ____________________________________________________________");
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println("      " + (i + 1) + ". " + list.get(i));
+                }
+                System.out.println("   ____________________________________________________________\n");
+            } else if (!input.isEmpty()) {
+                list.add(input);
+                System.out.println(response);
             }
         }
         scanner.close();
