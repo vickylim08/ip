483f90ab4abb18d494a154789909ca274fbe211c
Vicky Lim <vickylim078@gmail.com>
2026-08-19 15:59:48 +0800

    Rename the chatbot and add greeting

---
+++ src/main/java/Luna.java
@@ -0,0 +1,12 @@
+public class Luna {
+    public static void main(String[] args) {
+        String banner =
+                "____________________________________________________________\n" +
+                "Hello! I'm Luna.\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+        System.out.println(banner);
+    }
+}
